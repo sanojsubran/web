2cb967b7086c6bcfb1e1b8fa808e986d84c5e491
sanoj subran <sanoj009@gmail.com>
2017-01-02 06:14:49 +0530

    adding required class templates

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
 
-int main()
+int main( int argc, char* argv[] )
 {
     std::cout << "web" << std::endl;
+    if( 3 != argc ) {
+        std::cout << "Incorrect number of parameters" << std::endl;
+        std::cout << "Hint: web <url> <pattern>" << std::endl;
+        return 0;
+    } else {
+        std::string appname = argv[0];
+        std::string url = argv[1];
+        std::string pattern = argv[2];
+        std::cout << "App name: " << appname << std::endl;
+        std::cout << "Url: " << url << std::endl;
+        std::cout << "Pattern: " << pattern << std::endl;
+    }
     return 0;
 }
---
+++ src/page_retriever.h
@@ -0,0 +1,53 @@
+#pragma once
+
+class PageRetriever
+{
+
+};
+
+
+class PositionIndex
+{
+
+};
+
+
+class CustomSet
+{
+
+};
+
+
+class CustomLList
+{
+
+};
+
+
+class WordItem
+{
+
+};
+
+
+class PageIndex
+{
+
+};
+
+
+class HashTable
+{
+
+};
+
+class InvPageIndex
+{
+
+};
+
+
+class Search
+{
+
+};
